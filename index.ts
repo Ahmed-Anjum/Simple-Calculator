cc4f481cb54bd2199b41e3708a0e5b105e32b834
Ahmed-Anjum <ahmedanjum100@gmail.com>
2024-03-24 12:59:21 -0700

    Changed Console as The End

--- index.ts
+++ index.ts
@@ -2,8 +2,8 @@
 
 import inquirer from "inquirer";
 const answer = await inquirer.prompt([
-  { message: "Enter first number", type: "number", name: "firstNumber" },
-  { message: "Enter seond number", type: "number", name: "secondNumber" },
+  { message: "Enter your first number please", type: "number", name: "firstNumber" },
+  { message: "Enter your second number please", type: "number", name: "secondNumber" },
   {
     message: "Select one of these operators to perform action",
     type: "list",
@@ -20,4 +20,4 @@ else if (answer.operator === "Subtraction"){ console.log(+ answer.firstNumber - 
 else if (answer.operator === "Multiplication"){ console.log(+ answer.firstNumber * answer.secondNumber)}
 else if (answer.operator === "Division"){ console.log(+ answer.firstNumber / answer.secondNumber)}
 else {"Please select valid operator"}
-
+console.log("The End")
